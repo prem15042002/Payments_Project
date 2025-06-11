69d70c5469ed5ced2424b408c74db45938fdfd36
prem15042002 <bugatapremkumar@gmail.com>
2025-06-11 17:53:36 +0530

    Delete payments_project directory

--- payments_project/payments/templatetags/custom_filters.py
+++
@@ -1,8 +0,0 @@
-# payments/templatetags/custom_filters.py
-from django import template
-
-register = template.Library()
-
-@register.filter(name='add_class')
-def add_class(value, arg):
-    return value.as_widget(attrs={'class': arg})
--- payments_project/payments_project/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include # Make sure 'include' is imported here
-
-urlpatterns = [
-    path('admin/', admin.site.urls), # This is for Django's built-in admin
-    path('payments/', include('payments.urls')), # This routes /payments/ to your app's URLs
-    path('', include('payments.urls')), # OPTIONAL: This makes your app's list page the homepage
-]
